aca0ffccb69e3f18c0ef5773a7bca81cc3ac9662
Vincent Akena <vakena@outlook.com>
2023-01-12 20:31:25 +1100

    Updated comments

--- plex_media_rename.py
+++ plex_media_rename.py
@@ -4,8 +4,8 @@
 # DESCRIPTION
 
 # This script uses Filebot to mass rename Plex media, you will need a valid Filebot license
-# Place this script inside a movies library sample outputs of the file names have been provided
-# Use the Filebot commandline documentation to customise naming scheme
+# Place this script inside a movies library, sample outputs of the file names have been provided
+# Use the Filebot commandline documentation to customise the naming scheme to your liking
 
 import os
 import fnmatch
@@ -21,8 +21,8 @@ logging.basicConfig(level=logging.DEBUG, filename="log.log",
 def get_file_count(dir_path: str, pattern: str) -> int:
     """
     Args:
-        dir_path: The directory where the Plex media is stored
-        pattern: A regex pattern to match the file names.
+        dir_path: The location of the Plex library
+        pattern: A regex pattern to match media files (.mkv and .mp4)
     """
     count = 0
     regex = re.compile(pattern)
@@ -42,8 +42,8 @@ def get_files():
 
 
 def run_filebot_scan():
-    # Expected output: She's Not Here (2023) [2160p HDR10 x265 20Mbps] [Dolby Digital 5.1].mkv
-    # Expected output: She's Not Here (2023) [1080p x264 10Mbps] [AAC 2.0].mkv
+    # Expected output: She's Not Here (2023) (2160p HDR10 x265 20Mbps) (Dolby Digital 5.1).mkv
+    # Expected output: She's Not Here (2023) (1080p x264 10Mbps) (AAC 2.0).mkv
     logging.info("Filebot has started renaming your files...")
     os.system(
         'filebot -rename *.mkv *.mp4 --db TheMovieDB  --format "{n} ({y}) ({vf} {hdr} {vc} {bitrate}) ({aco} {channels})" -non-strict --apply refresh')
